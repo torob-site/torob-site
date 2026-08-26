d689ef45c38f61590101c80345c071c2bc30db6c
amirhosein <140063353+A9H8J9@users.noreply.github.com>
2026-08-26 20:26:37 +0330

    Update page.tsx

--- app/(main)/(account)/user/favorites/page.tsx
+++ app/(main)/(account)/user/favorites/page.tsx
@@ -1,12 +1,14 @@
-'use client'
+"use client";
 import ProductCard from "@/components/product-card";
 import { Spinner } from "@/components/ui/spinner";
-import { useGetUser, useGetUserAlerts, useGetUserFavorites } from "@/lib/apis"
+import { useGetUser, useGetUserAlerts, useGetUserFavorites } from "@/lib/apis";
 
 export default function Favorites() {
-  const { data, isPending, error } = useGetUserFavorites()
+  const { data: favorites = [], isPending, error } = useGetUserFavorites();
   const { data: user } = useGetUser();
-  const { data: alertIds = [] } = useGetUserAlerts(true, { enabled: !!user?.phone, });
+  const { data: alertIds = [] } = useGetUserAlerts(true, {
+    enabled: !!user?.phone && favorites.length > 0,
+  });
   const alertSet = new Set(alertIds);
   if (isPending) {
     return (
@@ -16,27 +18,34 @@ export default function Favorites() {
     );
   }
 
-  if (!data || data.length === 0) {
+  if (!favorites || favorites.length === 0) {
     return (
       <div className="w-full h-full flex flex-col items-center py-4 bg-[#ffffff] dark:bg-[#212b36]">
-        <img className="w-96" src='https://assets.torob.com/public/main/images/empty_likes.PNG' />
-        <p className="text-center text-sm mt-6 max-w-xs text-[#919eab]">محصولات محبوب خود را انتخاب کنید تا بعدا راحت‌تر پیدایشان کنید.</p>
+        <img
+          className="w-96"
+          src="https://assets.torob.com/public/main/images/empty_likes.PNG"
+        />
+        <p className="text-center text-sm mt-6 max-w-xs text-[#919eab]">
+          محصولات محبوب خود را انتخاب کنید تا بعدا راحت‌تر پیدایشان کنید.
+        </p>
       </div>
     );
   }
-  const products = data.map((product: any) => ({
+  const products = favorites.map((product: any) => ({
     ...product,
     is_favorite: true,
     is_alert: alertSet.has(product.id),
   }));
   return (
-    <div className="grid sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 grid-cols-2 gap-6">
-      {products.map((product: any) => (
-        <ProductCard
-          key={product.id}
-          product={product}
-        />
-      ))}
-    </div>
+    <>
+      <div className="px-8 mt-8">
+        <h1 className="font-bold text-2xl text-[#1e293b]">محبوب‌ها</h1>
+        <div className="grid sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5 grid-cols-2 gap-6 mt-5">
+          {products.map((product: any) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
