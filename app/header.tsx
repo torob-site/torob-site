8044ecf5f3d4b5f268c495d662d7599cbe360143
amirhosein <140063353+A9H8J9@users.noreply.github.com>
2026-08-26 20:22:51 +0330

    Update header.tsx

--- app/header.tsx
+++ app/header.tsx
@@ -1,13 +1,13 @@
-"use client";
 import Menu from "@/components/menu";
 import Theme from "@/components/theme";
 import User from "@/components/user";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 h-12 dark:bg-[#212b36] bg-[#ffffff] backdrop-blur-xl">
+    <header className="sticky top-0 z-50 h-12 bg-[#ffffff] backdrop-blur-xl dark:bg-[#212b36]">
       <div className="mx-auto flex h-full max-w-[1700px] items-center justify-between px-6">
         <Menu />
+
         <div className="flex items-center gap-6 text-white">
           <Theme />
           <User />
